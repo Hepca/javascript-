ac9c7ef68e1e641dd8d2059ca10d339bad6c35eb
Hepca <wangqiao@qiaoqiaodeMacBook-Pro.local>
2020-05-30 09:40:48 +0800

    快速排序高级算法

--- 3.快速排序.js
+++ 3.快速排序.js
@@ -34,3 +34,37 @@ var fastSort = function (arr) {
 var arr = [5,2,6,7,9,3,2]
 fastSort(arr)
 console.log(fastSort(arr), 'lalalal')
+
+// 快速排序(划分交换排序)
+var fastSortTwo = function (arr) {
+    // 数组指定两个位置进行交换
+    let swap = (arr, i , j) => {
+        let tmp = arr[i]
+        arr[i] = arr[j]
+        arr[j] = tmp
+    }
+    // 完成第一次划分交换
+    let findCenter = (arr, left, right) => {
+        let flag = arr[left]
+        let idx = left + 1
+        for (let i = idx; i <=right ; i++) {
+            if (arr[i] < flag) {
+                swap(arr, idx, i)
+                idx++
+            }
+        }
+        swap(arr, left, idx - 1)
+        return idx
+    }
+    // 递归排序
+    let sort = (arr, left, right) => {
+        if (left < right) {
+            // 找到中间元素
+            let center = findCenter(arr, left, right)
+            sort(arr,left,center - 1)
+            sort(arr,center, right)
+        }
+    }
+    sort(arr, 0 , arr.length - 1)
+    return arr
+}
